965774969decd215b9d052cb5e3eb58ec77163dc
abdulrahman <eng.abdoehab@gmail.com>
2020-06-11 01:31:44 +0200

    fix slider

--- app/src/main/java/com/example/profile/HomeFragment.java
+++ app/src/main/java/com/example/profile/HomeFragment.java
@@ -1,13 +1,11 @@
 package com.example.profile;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -26,11 +24,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-
 
 import java.util.ArrayList;
 
@@ -38,7 +32,7 @@ public class HomeFragment extends Fragment {
 
     private ArrayList<SliderItemModel> datasider;
     private DatabaseReference dbReferenceslider;
-    private ViewPager2 viewPager2 ;
+    private ViewPager2 viewPager2;
     private SliderAdapter sliderAdapter;
 
     private TabLayout tabLayout;
@@ -47,11 +41,16 @@ public class HomeFragment extends Fragment {
 
     public HomeFragment() {
     }
-
+    public static int pxToDp(int px) {
+        return (int) (px / Resources.getSystem().getDisplayMetrics().density);
+    }
+    public static int dpToPx(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_home , container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
         tabLayout = rootView.findViewById(R.id.tablayout);
         viewPager = rootView.findViewById(R.id.viewpager);
         viewadapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
@@ -61,24 +60,43 @@ public class HomeFragment extends Fragment {
 
         viewPager2 = rootView.findViewById(R.id.viewPagerSlider);
         showSlider(rootView);
-        viewPager2.setClipToPadding(false);
-        viewPager2.setClipChildren(false);
         viewPager2.setOffscreenPageLimit(3);
         viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
 
         CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
-        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new MarginPageTransformer(dpToPx(50)));
         compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            float pageMargin = getResources().getDimensionPixelOffset(R.dimen.pageMargin);
+            float pageOffset = getResources().getDimensionPixelOffset(R.dimen.offset);
             @Override
             public void transformPage(@NonNull View page, float position) {
-                float r = 1- Math.abs(position);
-                page.setScaleY(0.085f + r * 0.15f);
+                String TAG = "VIEWPAGER_POS";
+                float myOffset = position * -(2 * pageOffset + pageMargin);
+                if (position < -1) {
+                    page.setTranslationX(-myOffset);
+                } else if (position <= 1) {
+                    float scaleFactor = Math.max(0.7f, 1 - Math.abs(position - 0.14285715f));
+                    Log.d(TAG, "transformPageVALUE: " + scaleFactor);
+                    page.setTranslationX(myOffset);
+                    page.setScaleY(scaleFactor);
+                    page.setAlpha(scaleFactor);
+                } else {
+                    page.setAlpha(0);
+                    page.setTranslationX(myOffset);
+                }
+
+
+
+
+                float r = 1 - Math.abs(position);
+                float scale = 0.085f + r * 0.15f;
+                Log.d(TAG, "transformPagePOS: " + position);
+                Log.d(TAG, "transformPageRADIUS: " + r);
+                Log.d(TAG, "transformPageVALUE: " + scale);
+//                page.setScaleY(scale);
             }
         });
         viewPager2.setPageTransformer(compositePageTransformer);
-        
-
-
 
 
 //        RVAdapterCategory.ImageClickListener listenerImage = new RVAdapterCategory.ImageClickListener() {
@@ -128,20 +146,20 @@ public class HomeFragment extends Fragment {
 //        recyclerViewImages.setAdapter(adapterImages);
 //
 
-        RVAdapterCategory adapter = new RVAdapterCategory( getList(),listener );
-        RecyclerView recyclerView = (RecyclerView)rootView.findViewById(R.id.similarItem_recyclerView);
-        recyclerView.setLayoutManager(new GridLayoutManager(getActivity() , 2));
+        RVAdapterCategory adapter = new RVAdapterCategory(getList(), listener);
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.similarItem_recyclerView);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
         recyclerView.setAdapter(adapter);
-        
 
-        RVAdapterRecentlyView adapterRecView = new RVAdapterRecentlyView( getListofRecentlyView(),listenerRecView );
-        RecyclerView recyclerViewRecView = (RecyclerView)rootView.findViewById(R.id.recently_view_recyclerView);
-        recyclerViewRecView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false) );
+
+        RVAdapterRecentlyView adapterRecView = new RVAdapterRecentlyView(getListofRecentlyView(), listenerRecView);
+        RecyclerView recyclerViewRecView = (RecyclerView) rootView.findViewById(R.id.recently_view_recyclerView);
+        recyclerViewRecView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
         recyclerViewRecView.setAdapter(adapterRecView);
 
-        RVAdapterRecentFavorite adapterRecFav = new RVAdapterRecentFavorite( getListofRecentlyFav(),listenerRecFav );
-        RecyclerView recyclerViewRecFav= (RecyclerView)rootView.findViewById(R.id.recently_fav_recyclerView);
-        recyclerViewRecFav.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false) );
+        RVAdapterRecentFavorite adapterRecFav = new RVAdapterRecentFavorite(getListofRecentlyFav(), listenerRecFav);
+        RecyclerView recyclerViewRecFav = (RecyclerView) rootView.findViewById(R.id.recently_fav_recyclerView);
+        recyclerViewRecFav.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
         recyclerViewRecFav.setAdapter(adapterRecFav);
 
 //        RVAdapterOtherCategory adapterOtherCategory = new RVAdapterOtherCategory( getListofOtherCateg(),listenerotherCateg );
@@ -159,16 +177,18 @@ public class HomeFragment extends Fragment {
         dbReferenceslider.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     SliderItemModel item = snapshot.getValue(SliderItemModel.class);
                     datasider.add(item);
                 }
-                SliderAdapter Adapter = new SliderAdapter(datasider , viewPager2);
-                viewPager2.setAdapter(Adapter);
+                 sliderAdapter = new SliderAdapter(datasider, viewPager2);
+                viewPager2.setAdapter(sliderAdapter);
 
-            }   @Override
+            }
+
+            @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Toast.makeText(getActivity() , databaseError.getMessage(), Toast.LENGTH_LONG).show();
+                Toast.makeText(getActivity(), databaseError.getMessage(), Toast.LENGTH_LONG).show();
 
             }
         });
@@ -177,47 +197,51 @@ public class HomeFragment extends Fragment {
 
     private ArrayList<ItemModel> getList() {
         final ArrayList<ItemModel> item = new ArrayList<ItemModel>();
-        item.add(new ItemModel("Item Full Name", "$150",R.drawable.grid_one ));
+        item.add(new ItemModel("Item Full Name", "$150", R.drawable.grid_one));
         item.add(new ItemModel("Item Full Name", "$150", R.drawable.grid_two));
         item.add(new ItemModel("Item Full Name", "$150", R.drawable.grid_three));
-        item.add(new ItemModel("Item Full Name", "$150", R.drawable.grid_four ));
+        item.add(new ItemModel("Item Full Name", "$150", R.drawable.grid_four));
         return item;
     }
+
     private ArrayList<Integer> getListofImages() {
         final ArrayList<Integer> image = new ArrayList<Integer>();
         image.add(R.drawable.ring_one);
-        image.add( R.drawable.ring_two);
+        image.add(R.drawable.ring_two);
         image.add(R.drawable.ring_three);
-        image.add( R.drawable.ring_four);
+        image.add(R.drawable.ring_four);
         return image;
     }
+
     private ArrayList<Integer> getListofOtherCateg() {
         final ArrayList<Integer> image = new ArrayList<Integer>();
         image.add(R.drawable.grid_three);
-        image.add( R.drawable.test_image);
+        image.add(R.drawable.test_image);
         image.add(R.drawable.grid_three);
-        image.add( R.drawable.grid_one);
+        image.add(R.drawable.grid_one);
         return image;
     }
+
     private ArrayList<Integer> getListofRecentlyView() {
         final ArrayList<Integer> image = new ArrayList<Integer>();
         image.add(R.drawable.grid_two);
         image.add(R.drawable.grid_three);
         image.add(R.drawable.grid_four);
-        image.add( R.drawable.test_image);
-        image.add( R.drawable.grid_one);
-        image.add( R.drawable.test_image);
+        image.add(R.drawable.test_image);
+        image.add(R.drawable.grid_one);
+        image.add(R.drawable.test_image);
 
         return image;
     }
+
     private ArrayList<Integer> getListofRecentlyFav() {
         final ArrayList<Integer> image = new ArrayList<Integer>();
         image.add(R.drawable.grid_two);
         image.add(R.drawable.grid_three);
         image.add(R.drawable.grid_four);
-        image.add( R.drawable.test_image);
-        image.add( R.drawable.grid_one);
-        image.add( R.drawable.test_image);
+        image.add(R.drawable.test_image);
+        image.add(R.drawable.grid_one);
+        image.add(R.drawable.test_image);
 
 
         return image;
